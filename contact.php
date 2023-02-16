f1852d0840e73d34e64e680974a121e7271bf07f
Sreenidh26 <67754320+Sreenidh26@users.noreply.github.com>
2023-02-15 22:25:14 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['firstname'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+ 
+$email_from = "$visitor_email.\n";
+ 
+$email_subject = "$subject\n";
+ 
+$email_body = "Name: $name.\n".
+				"Message: $message\n";
+ 
+$to = "codelybusiness@gmail.com";
+ 
+ 
+$headers = "From: $email_from \r\n";
+ 
+$headers .= "Reply-To: $visitor_email \r\n";
+ 
+mail($to,$email_subject,$email_body,$headers);
+ 
+header("Location: index.html");
+ 
+?>
